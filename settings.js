90bfed15c5caca7684db6d6e48e8107790c5daf4
jackteaplaybot <157235040+jackteaplaybot@users.noreply.github.com>
2024-07-19 12:00:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.mtl_language = false // multi language i hman dawn chuan true ah dah tur
 global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chuan https://herbert70.blogspot.com/p/language-code-for-hbwabot.html?m=1 he link ah hian en thei a ni ang
 
 //info
-global.owner = ['916009259402'] // I phone number in thlak rawh
+global.owner = ['918798567517'] // I phone number in thlak rawh
 global.ownernumber = '918798567517' // I phone number in thlak rawh
 global.ownername = "Zo Robot" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
